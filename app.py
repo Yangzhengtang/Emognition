2b9769ea56e09e003b12785dbc0018f8a3caa94a
huyz97 <302995105@qq.com>
2019-06-21 13:38:21 +0800

    添加了开放端口为8090

--- app.py
+++ app.py
@@ -359,5 +359,5 @@ def showTestResult():
     return render_template('testResult.html')
 
 if __name__ == '__main__':
-    app.run(threaded=True,host="0.0.0.0")
+    app.run(threaded=True,host="0.0.0.0",port=8090)
 
