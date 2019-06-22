1a7bae36f93beb50eba754743ffd4938506d8f28
huyz97 <302995105@qq.com>
2019-06-22 11:06:54 +0800

    fix bugs

--- Recognize/Recognize.py
+++ Recognize/Recognize.py
@@ -2,15 +2,12 @@ import numpy as np
 import cv2
 import sys
 import json
-import time
-import os
-# import copy
 from keras.models import model_from_json
 
 class Recognition():
-    def __init__(self,jsonfile,h5model_file,xml_file):
+    def __init__(self,jsonfile,h5model_file,xml_file,labels):
         self.img_size = 48
-        self.emotion_labels = ['angry', 'disgust:', 'fear', 'happy', 'sad', 'surprise', 'neutral']
+        self.emotion_labels = labels
         self.num_class = len(self.emotion_labels)
         json_file = open(jsonfile)
         loaded_model_json = json_file.read()
@@ -87,20 +84,3 @@ class Recognition():
             cv2.imwrite(out_path, img)
             cv2.waitKey(0)
 
-
-def recognition(photo,model):
-    '''
-    利用训练好的模型对photo进行识别
-    :param photo:文件格式，需要识别的照片
-    :param model:文件格式，在前端选择的.h5模型
-    :return:photofile_labeled 打好标签并框选出人脸的照片
-    '''
-
-
-def train(label,photofile):
-    '''
-    先把photofile和label存入数据库，并放入某个文件夹下，把label和photofile放入网络训练，也是调用os.system()
-    :param label: 目前先通过下拉栏给用户有限的选项
-    :param photofile: 待放入网络训练的数据
-    :return: new_photofile_labeled（通过新训练好的网络对刚刚用户输入的Photofile的预测，和用户给的标签可以进行对比）,new_accuary（返回新的准确度数组，现在可能实现不了，先放着）
-    '''
